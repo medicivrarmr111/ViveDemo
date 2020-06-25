da19010b49411e2e1cb90529dcb6fe61668f190d
medicivrarmr111 <67042254+medicivrarmr111@users.noreply.github.com>
2020-06-25 14:54:15 +0900

    스크립트 수정

---
+++ Assets/02.Scripts/ViveController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveController : MonoBehaviour
+{
+
+
+    // 컨트롤러 정의
+    public SteamVR_Input_Sources leftHand;
+    public SteamVR_Input_Sources rightHand;
+    public SteamVR_Input_Sources any;
+
+
+    // 컨트롤러 입력값 정의
+    public SteamVR_Action_Boolean trigger;
+    
+    private void Awake() 
+    {
+        trigger = SteamVR_Actions.default_InteractUI;
+    }
+
+    
+
+    
+    void Update()
+    {
+        if(SteamVR_Actions.default_InteractUI.GetStateDown(leftHand))
+        {
+            Debug.Log("Clicked Trigger Button");
+        }
+    }
+}
