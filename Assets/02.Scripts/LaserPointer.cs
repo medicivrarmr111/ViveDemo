d89dd8f927428ca8291d69550bedb90bb5cd5010
medicivrarmr111 <67042254+medicivrarmr111@users.noreply.github.com>
2020-06-25 16:56:13 +0900

    update code

---
+++ Assets/02.Scripts/LaserPointer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class LaserPointer : MonoBehaviour
+{
+    private SteamVR_Behaviour_Pose pose;
+    private SteamVR_Input_Sources hand;
+    private SteamVR_Action_Boolean teleport;
+
+    //라인렌더러 속성 변수
+    private LineRenderer line;
+    [Range(3.0f, 10.0f)]
+    public float distance = 5.0f;
+    public Color defaultColor = Color.green;
+    public Color clickedColor = Color.blue;
+
+    void Start()
+    {
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+        hand = SteamVR_Input_Sources.LeftHand;
+        teleport = SteamVR_Actions.default_Teleport;
+        
+        CreateLine();
+    }
+
+    void CreateLine()
+    {
+        line = this.gameObject.AddComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, new Vector3(0, 0, distance));
+
+        // Un light 조명없어도 라이트
+        line.material = new Material(Shader.Find("Unlit/Color"));
+        line.material.color = defaultColor;
+        line.startWidth = 0.05f;
+        line.endWidth   = 0.005f;
+    }
+
+
+}
