518b11c0807591f0c934444642625b383d79e386
medicivrarmr111 <67042254+medicivrarmr111@users.noreply.github.com>
2020-06-25 15:50:32 +0900

    haptic 기능 추가

--- Assets/02.Scripts/ViveController.cs
+++ Assets/02.Scripts/ViveController.cs
@@ -19,6 +19,8 @@ public class ViveController : MonoBehaviour
     public SteamVR_Action_Boolean trackPadClick = SteamVR_Actions.default_Teleport;
     public SteamVR_Action_Boolean trackPadTouch = SteamVR_Actions.default_TrackPadTouch;
     public SteamVR_Action_Vector2 trackPadPosition = SteamVR_Actions.default_TrackPadPosition;
+    
+    private SteamVR_Action_Vibration haptic = SteamVR_Actions.default_Haptic;
 
     private void Awake() 
     {
@@ -31,6 +33,7 @@ public class ViveController : MonoBehaviour
         if(trigger.GetStateDown(leftHand))
         {
             Debug.Log("Clicked Trigger Button");
+            haptic.Execute(0.2f, 0.3f, 120.0f, 0.5f, leftHand);
         }
 
         // 오른손 컨트롤러의 트리거 버튼을 릴리스했을 때 발생
