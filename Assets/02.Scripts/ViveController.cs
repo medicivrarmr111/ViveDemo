52914fc9b9269827d363058b541cdead555f05f4
medicivrarmr111 <67042254+medicivrarmr111@users.noreply.github.com>
2020-06-25 15:44:32 +0900

    중복된 코드 삭제함

--- Assets/02.Scripts/ViveController.cs
+++ Assets/02.Scripts/ViveController.cs
@@ -15,20 +15,42 @@ public class ViveController : MonoBehaviour
 
     // 컨트롤러 입력값 정의
     public SteamVR_Action_Boolean trigger;
-    
+    // 트랙패드 관련 입력값 정의
+    public SteamVR_Action_Boolean trackPadClick = SteamVR_Actions.default_Teleport;
+    public SteamVR_Action_Boolean trackPadTouch = SteamVR_Actions.default_TrackPadTouch;
+    public SteamVR_Action_Vector2 trackPadPosition = SteamVR_Actions.default_TrackPadPosition;
+
     private void Awake() 
     {
         trigger = SteamVR_Actions.default_InteractUI;
     }
 
-    
-
-    
     void Update()
     {
-        if(SteamVR_Actions.default_InteractUI.GetStateDown(leftHand))
+        // 왼손 컨트롤러의 트리거 버튼을 클릭했을 때 발생
+        if(trigger.GetStateDown(leftHand))
         {
             Debug.Log("Clicked Trigger Button");
         }
+
+        // 오른손 컨트롤러의 트리거 버튼을 릴리스했을 때 발생
+        if(trigger.GetStateUp(rightHand))
+        {
+            Debug.Log("Release Trigger Button");
+        }
+
+        // 트랙패드 클릭
+        if(trackPadClick.GetStateDown(any))
+        {
+            Debug.Log("TrackPad Click");
+        }
+
+        if(trackPadTouch.GetState(any))
+        {
+            Vector2 pos = trackPadPosition.GetAxis(any);
+            Debug.Log($"Touch Pos x={pos.x}/y={pos.y}");
+        }
+
+        
     }
 }
